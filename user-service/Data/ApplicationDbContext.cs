d2b1a6567e3f084d67d814764d6e418e274897ab
julioandre275 <julioandre275@gmail.com>
2023-05-02 14:47:53 +0200

    rebase commits/branch

--- user-service/Data/ApplicationDbContext.cs
+++ user-service/Data/ApplicationDbContext.cs
@@ -1,9 +1,10 @@
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using user_service.Models;
 
 namespace user_service.Data;
 
-public class ApplicationDbContext:DbContext
+public class ApplicationDbContext:IdentityDbContext
 {
     public ApplicationDbContext(DbContextOptions options) : base(options)
     {
