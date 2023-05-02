ab62d812327b25a10c38f7bfa8c1c8e94f085c18
julioandre275 <julioandre275@gmail.com>
2023-05-02 09:03:15 +0200

    adding ci/cd

---
+++ user-service/Controllers/userServiceController.cs
@@ -0,0 +1,108 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using user_service.Data;
+using user_service.Models;
+
+namespace user_service.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class userServiceController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public userServiceController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/userService
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<UserEntity>>> GetUsers()
+        {
+            return await _context.Users.ToListAsync();
+        }
+
+        // GET: api/userService/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<UserEntity>> GetUserEntity(int id)
+        {
+            var userEntity = await _context.Users.FindAsync(id);
+
+            if (userEntity == null)
+            {
+                return NotFound();
+            }
+
+            return userEntity;
+        }
+
+        // PUT: api/userService/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutUserEntity(int id, UserEntity userEntity)
+        {
+            if (id != userEntity.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(userEntity).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!UserEntityExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/userService
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<UserEntity>> PostUserEntity(UserEntity userEntity)
+        {
+            _context.Users.Add(userEntity);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetUserEntity", new { id = userEntity.Id }, userEntity);
+        }
+
+        // DELETE: api/userService/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteUserEntity(int id)
+        {
+            var userEntity = await _context.Users.FindAsync(id);
+            if (userEntity == null)
+            {
+                return NotFound();
+            }
+
+            _context.Users.Remove(userEntity);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool UserEntityExists(int id)
+        {
+            return _context.Users.Any(e => e.Id == id);
+        }
+    }
+}
