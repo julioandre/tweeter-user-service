2c9923eb7cac6b90bc83114b5035036c40216ee9
julioandre275 <julioandre275@gmail.com>
2023-05-08 08:10:41 +0200

    Created Kuberneetes Deployment for Kafka

---
+++ user-service/Migrations/20230508053929_minor_change.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace user_service.Migrations
+{
+    /// <inheritdoc />
+    public partial class minor_change : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "LaginDate",
+                table: "AspNetUsers",
+                newName: "LoginDate");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "LoginDate",
+                table: "AspNetUsers",
+                newName: "LaginDate");
+        }
+    }
+}
