e8d0df3e24de166c235c4dde591e9f703a5e79f5
julioandre275 <julioandre275@gmail.com>
2023-05-23 15:37:57 +0200

    Created new docker image

--- user-service/Controllers/userserviceController.cs
+++ user-service/Controllers/userserviceController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.IdentityModel.Tokens;
+using user_service.Data;
 using user_service.Models;
 
 
@@ -21,14 +22,16 @@ namespace user_service.Controllers
         private readonly ILogger<userserviceController>_logger;
         private readonly IMapper _mapper;
         private IConfiguration _configuration;
+        private ApplicationDbContext _dbContext;
 
-        public userserviceController(UserManager<UserEntity> userManager, SignInManager<UserEntity> signInManager, ILogger<userserviceController> logger, IMapper mapper, IConfiguration configuration)
+        public userserviceController(UserManager<UserEntity> userManager, SignInManager<UserEntity> signInManager, ILogger<userserviceController> logger, IMapper mapper, IConfiguration configuration, ApplicationDbContext dbContext)
         {
             _userManager = userManager;
             _signInManager = signInManager;
             _logger = logger;
             _mapper = mapper;
             _configuration = configuration;
+            _dbContext = dbContext;
         }
         // From Body to prevent passing sensitive info in the url headers
         [AllowAnonymous]
@@ -80,7 +83,8 @@ namespace user_service.Controllers
                     return Unauthorized(userDTO);
                 }
 
-                var token = GenerateToken(userDTO);
+                var user = GetUser(userDTO.Email);
+                var token = GenerateToken(user);
                 return Ok(token);
             }
             catch (Exception e)
@@ -91,20 +95,28 @@ namespace user_service.Controllers
             }
         }
 
-        private string GenerateToken(LoginUserDTO userDTO)
+        private string GenerateToken(UserEntity user)
         {
             var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]));
             var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
 
             var claims = new[]
             {
-                new Claim(ClaimTypes.Email, userDTO.Email),
+                new Claim(ClaimTypes.Email, user.Email),
+                new Claim(ClaimTypes.NameIdentifier, user.Id),
+                new Claim(ClaimTypes.GivenName, user.FirstName),
+                new Claim(ClaimTypes.Surname, user.LastName)
                 
 
 
             };
             var token = new JwtSecurityToken(_configuration["JwtIssuer"], _configuration["JwtAudience"], claims,expires: DateTime.Now.AddMinutes(25),signingCredentials:credentials);
             return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
+        private UserEntity GetUser(string username)
+        {
+           return _dbContext.Users.FirstOrDefault(u=>u.UserName == username);
         }
 
 
