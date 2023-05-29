5d0b7a76b025f6f7fc7476f459d595407efa78bc
julioandre275 <julioandre275@gmail.com>
2023-05-29 15:42:19 +0200

    Fixing Scoped Services Error

--- user-service/Messaging/Consumers.cs
+++ user-service/Messaging/Consumers.cs
@@ -2,29 +2,27 @@ using System.Diagnostics;
 using Confluent.Kafka;
 using Newtonsoft.Json;
 using user_service.Services;
+using NotImplementedException = System.NotImplementedException;
 
 namespace user_service.Messaging;
 
-public class Consumers:IConsumers
+public class Consumers:IHostedService
 {
     private readonly string bootstrapServers = "localhost:9092";
     private IUserService _userService;
     private IProducers _producers;
-
-    public Consumers(IUserService userService, IProducers producers)
+    private IServiceProvider _serviceProvider;
+    public Consumers(IServiceProvider serviceProvider)
     {
-        _userService = userService;
-        _producers = producers;
+        _serviceProvider = serviceProvider;
+
     }
     public Task StartAsync(CancellationToken cancellationToken)
     {
-        throw new NotImplementedException();
-    }
-
-   
-
-    public Task GetIdAsync(string topic, CancellationToken cancellationToken)
-    { 
+        using var scope = _serviceProvider.CreateScope();
+        // For scoped services error
+        _userService = scope.ServiceProvider.GetRequiredService<IUserService>();
+        string topic = "followTopic";
         string groupId = "test_group";
         var config = new ConsumerConfig {GroupId = groupId, BootstrapServers = bootstrapServers, AutoOffsetReset = AutoOffsetReset.Earliest};
         try
@@ -40,19 +38,20 @@ public class Consumers:IConsumers
                         var consumer = consumerBuilder.Consume(cancelToken.Token);
                         var orderRequest = JsonConvert.DeserializeObject<string>(consumer.Message.Value);
                         var userEntity = _userService.GetUser(orderRequest);
-                        _producers.SendId("userTopic", orderRequest);
-                        Debug.WriteLine("Sending ID to followService");
+                        Debug.WriteLine("Sending ID to followService" + userEntity.Id);
                     }
                 }catch (OperationCanceledException) {
                     consumerBuilder.Close();
+                }
             }
-        }
         
-    }catch (Exception ex) {
+        }catch (Exception ex) {
             System.Diagnostics.Debug.WriteLine(ex.Message);
         }
         return Task.CompletedTask;
+        
     }
+    
     public Task StopAsync(CancellationToken cancellationToken) {
         return Task.CompletedTask;
     }
